681f9fe294a056d68fd08d453ff8eadf3ce078ab
kathirviswa <kathirviswa57@gmail.com>
2025-03-11 13:23:12 +0530

    f

--- flipcart-clone/src/App.jsx
+++ flipcart-clone/src/App.jsx
@@ -1,14 +1,46 @@
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import './App.css'
 
-function App() {
+
+//Components
+import Home from './Pages/Home/Home'
+import Products from './Pages/Products/Products'
+import ProductDetails from './Pages/ProductDetails/ProductDetails'
+
+
+//App Component
+export default function App() {
 
 
   return (
-    <>
-            
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <h1>FlipCart Clone</h1>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/productdetails/:productId" element={<ProductDetails />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
+// function App() {
 
-export default App
+
+//   return (
+//     <>
+//         <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/products" element={<Products />} />
+//           <Route path="/productdetails/:productId" element={<ProductDetails />} />
+//         </Routes>
+//         </BrowserRouter>    
+//     </>
+//   )
+// }
+
+// export default App
