77eaa4c82d932f7bb2a4ea04a78be7e1f82e8ffe
kathirviswa <kathirviswa57@gmail.com>
2025-03-11 13:53:39 +0530

    n

--- flipcart-clone/src/App.jsx
+++ flipcart-clone/src/App.jsx
@@ -1,37 +1,31 @@
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
 
+// Components
+import Navbar from './Components/Navbar';
 
-
-
-//Components
-import Navbar from './Components/Navbar'
-
-//PagesComponents
-import Home from './pages/Home/Home'
-import Products from './pages/Products/Products'
-import ProductDetails from './pages/ProductDetails/ProductDetails'
-import Cart from './pages/Cart/Cart'
-
-
+// Pages
+import Home from './pages/Home/Home';
+import Products from './pages/Products/Products';
+import ProductDetails from './pages/ProductDetails/ProductDetails';
+import Cart from './pages/Cart/Cart';
 
 export default function App() {
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>FlipCart Clone</h1>
-        <Navbar />  {/* Navbar component */}
-      </header>
- 
-      <BrowserRouter>
+    <BrowserRouter> {/* Wrap the entire app */}
+      <div className="App">
+        <header className="App-header">
+          <h1>FlipCart Clone</h1>
+          <Navbar /> {/* Navbar inside BrowserRouter */}
+        </header>
+        
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/productdetails/:productId" element={<ProductDetails />} />
         </Routes>
-      </BrowserRouter>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
