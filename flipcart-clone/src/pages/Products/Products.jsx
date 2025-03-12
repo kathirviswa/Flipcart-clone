0cc7a865e97e1e74eec6e8d9919e1af54d0ba76b
kathirviswa <kathirviswa57@gmail.com>
2025-03-12 13:27:34 +0530

    g

--- flipcart-clone/src/pages/Products/Products.jsx
+++ flipcart-clone/src/pages/Products/Products.jsx
@@ -5,18 +5,14 @@ const Products = () => {
     <>
     <div className='product-list'>
 <div className='product-cart'>
-  <div className='product'>
-    <img src='https://i.pinimg.com/736x/3f/b9/2b/3fb92b0d2b489959a59896d4f90018f2.jpg' alt='Product 1' />
-    <div className='product-details'>
-      <h2>Product 1</h2>
-      <p>$100</p>
-      <button>Add to Cart</button>
+<img src='image' alt='image-title'/>
+    <h1>product title</h1>
+    
     </div>
   </div>
-</div>
-    </div>
+
+    
     </>
   )
 }
-
 export default Products
