c78e98fee0589d9e9ace33a973eda2c4c697f25f
kathirviswa <kathirviswa57@gmail.com>
2025-03-12 17:58:40 +0530

    cart complete api

--- flipcart-clone/src/pages/Cart/Cart.jsx
+++ flipcart-clone/src/pages/Cart/Cart.jsx
@@ -2,33 +2,32 @@ import React from 'react'
 
 const Cart = () => {
   return (
-    <div className='cart-page-container p-8  w-[800px] items-center '>
-    <div className="cart-container  bg-slate-50 rounded-lg shadow-xl p-8 w-full max-w-4xl">
+    <div className="wrapper">
+    <div className='cart-page-container p-8  w-[800px] '>
+    <div className="cart-container justify-around bg-slate-50 rounded-lg shadow-xl p-8 w-full max-w-4xl">
       <h1 > Your Cart</h1>
       <div className="cart-items flex items-start border-b-1 p-4 gap-4">
        <img src='' alt='Image-title' className='flex-0 w-25 object-contain'/>
-       <div className="cart-item-details">
-        <h2>Product Title</h2>
+       <div className="cart-item-details grow-1">
+        <h2 className='font-bold text-lg text-gray-700 hover:text-[#2874f0]'>Product Title</h2>
         <p>Price: $99.99</p>
        
         <div className="">
-          <input type='number' min='1' className='w-15 mr-1 p-1 '/>
-        <button className="update-button bg-[#DC5F00] text-amber-50 border-none px-2 p-1 rounded-sm cursor-pointer mr-1">+</button>
+          <input type='number' min='1' className='w-15 mr-1 p-1'/>
+        <button className="update-button bg-[#DC5F00] text-amber-50 border-none px-2 p-1 rounded-sm cursor-pointer mr-1 ">+</button>
         <button className="remove-button bg-[#DC5F00] text-amber-50 border-none px-2 p-1 rounded-sm cursor-pointer mr-1">-</button>
        </div>
       </div>
       </div>
       {/* cart total */}
-      <div className="cart-total text-right mr-1">
+      <div className="cart-total text-right mr-1 text-xl font-semibold" >
      <p >Total : $200</p>
       </div>
      <div className="">
-        <button className="check-button bg-[#DC5F00] text-amber-50 border-none p-3  px-2 rounded-sm w-full">Checkout</button>
-        <div className="flex items-center gap-4">
-        <input type='checkbox' className='w-6 h-6' />
-        <label className="text-sm text-gray-700">Save for later</label>
+        {/* <button className="check-button bg-[#DC5F00] text-amber-50 border-none p-3  px-2 rounded-sm w-full">Checkout</button>
+        <div> */}
+        <button className="continue-button bg-[#DC5F00] text-amber-50 border-none p-3 px-2 rounded-sm w-full">Continue Shopping</button>
         </div>
-        <button className="continue-button bg-[#DC5F00] text-amber-50 border-none p-3  px-2 rounded-sm w-full">Continue Shopping</button>
         </div>
     </div>
     </div>
--- flipcart-clone/src/pages/Products/Products.jsx
+++ flipcart-clone/src/pages/Products/Products.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Products = () => {
+const [Products, setProducts]= useState([])
+useEffect (() => {
+  const fetchProducts =async() =>{
+  const response = await fetch('https://fakestoreapi.com/products')
+  const data = await response.json();
+   setProducts(data);
+  //  console.log(data);
+}
+fetchProducts();
+},[])
+
+
   return (
     <>
   <main className="max-w-7xl mx-auto px-4 py-6">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {/* Example Product Cards */}
        
+           {Products.map(Products=>(
             <div  className="bg-white p-4 rounded-lg shadow hover:shadow-lg transition-shadow">
-              <div className="aspect-square bg-gray-100 rounded-lg mb-4">
-                <img src="" alt="Product" className="w-full h-full object-cover rounded-lg" />
-              </div>
-
-              <h3 className="font-medium text-gray-800">Product Name</h3>
-              <p className="text-sm text-gray-500">Category</p>
-              <div className="mt-2 flex items-center justify-between">
-                <span className="font-bold text-gray-800">₹499</span>
-                <span className="text-green-700 text-sm">40% off</span>
-    
-                <button className="px-4 py-2 text-sm font-medium text-white bg-orange-600 rounded-lg">Add to Cart</button>
-              </div>
+            <div className="aspect-square bg-gray-100 rounded-lg mb-4">
+              <img src= {Products.image} alt="Product" className="w-full h-full object-contain rounded-lg" />
             </div>
+
+            <h3 className="font-medium text-gray-800">{Products.title}</h3>
+             <p className="text-sm text-gray-600">{Products.category}</p> 
+            <div className="mt-2 flex items-center justify-between">
+           <span className="font-bold text-gray-800">${Products.price}</span>
+              <span className="text-green-700 text-sm">40% off</span>
+  
+              <button className="px-4 py-2 text-sm font-medium text-white bg-orange-600 rounded-lg">Add to Cart</button>
+            </div>
+          </div>
+           ))} 
        
         </div>
       </main>
