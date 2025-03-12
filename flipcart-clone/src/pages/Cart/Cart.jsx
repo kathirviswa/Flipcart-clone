5f5f9ab124827b0a826e002f49bf83304f739289
kathirviswa <kathirviswa57@gmail.com>
2025-03-12 16:08:13 +0530

    ddf

--- flipcart-clone/src/pages/Cart/Cart.jsx
+++ flipcart-clone/src/pages/Cart/Cart.jsx
@@ -6,31 +6,29 @@ const Cart = () => {
     <div className="cart-container  bg-slate-50 rounded-lg shadow-xl p-8 w-full max-w-4xl">
       <h1 > Your Cart</h1>
       <div className="cart-items flex items-start border-b-1 p-4 gap-4">
-       <img src='' alt='Image-title' className=''/>
+       <img src='' alt='Image-title' className='flex-0 w-25 object-contain'/>
        <div className="cart-item-details">
         <h2>Product Title</h2>
         <p>Price: $99.99</p>
+       
         <div className="">
-          <input type='number' min='1'/>
-        <button className="update-button">+</button>
-        <button className="remove-button">-</button>
+          <input type='number' min='1' className='w-15 mr-1 p-1 '/>
+        <button className="update-button bg-[#DC5F00] text-amber-50 border-none px-2 p-1 rounded-sm cursor-pointer mr-1">+</button>
+        <button className="remove-button bg-[#DC5F00] text-amber-50 border-none px-2 p-1 rounded-sm cursor-pointer mr-1">-</button>
        </div>
       </div>
       </div>
-
-      
-      <div className="cart-total">
-     <p>Total : $200</p>
+      {/* cart total */}
+      <div className="cart-total text-right mr-1">
+     <p >Total : $200</p>
       </div>
-      <div className="cart-buttons">
-        <button className="cart-button">Checkout</button>
-        <button className="cart-button">Continue Shopping</button>
-      </div>
-      {/* <div className="cart-footer">
-        <p>�� 2025 Flipkart.com</p>
-      </div> */}
+     <div className="">
+        <button className="check-button bg-[#DC5F00] text-amber-50 border-none p-3  px-2 rounded-sm w-full">Checkout</button>==
+        <button className="continue-button bg-[#DC5F00] text-amber-50 border-none p-3  px-2 rounded-sm w-full">Continue Shopping</button>
+        </div>
     </div>
     </div>
+   
   )
 }
 
