94cfc26ce12c4f1cf68bdddd1e0725edc1787ac0
kathirviswa <kathirviswa57@gmail.com>
2025-03-12 11:50:05 +0530

    df

--- flipcart-clone/src/pages/Products/Products.jsx
+++ flipcart-clone/src/pages/Products/Products.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <>
+    <div className='product-list'>
+<div className='product-cart'>
+  <div className='product'>
+    <img src='https://i.pinimg.com/736x/3f/b9/2b/3fb92b0d2b489959a59896d4f90018f2.jpg' alt='Product 1' />
+    <div className='product-details'>
+      <h2>Product 1</h2>
+      <p>$100</p>
+      <button>Add to Cart</button>
+    </div>
+  </div>
+</div>
+    </div>
+    </>
   )
 }
 
