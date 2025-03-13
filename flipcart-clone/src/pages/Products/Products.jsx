7b0ebb4e5da7915be7bd2f089913bc8b6d829550
kathirviswa <kathirviswa57@gmail.com>
2025-03-13 12:48:10 +0530

    complete product section

--- flipcart-clone/src/pages/Products/Products.jsx
+++ flipcart-clone/src/pages/Products/Products.jsx
@@ -4,10 +4,10 @@ const Products = () => {
 const [Products, setProducts]= useState([])
 useEffect (() => {
   const fetchProducts =async() =>{
-  const response = await fetch('https://api.escuelajs.co/api/v1/products')
+  const response = await fetch('https://fakestoreapi.com/products')
   const data = await response.json();
    setProducts(data);
-    // console.log(data);
+  //  console.log(data);
 }
 fetchProducts();
 },[])
@@ -22,13 +22,14 @@ fetchProducts();
            {Products.map(Products=>(
             <div  className="bg-white p-4 rounded-lg shadow hover:shadow-lg transition-shadow">
             <div className="aspect-square bg-gray-100 rounded-lg mb-4">
-              <img src= {Products.image} alt="Product" className="w-full h-full object-contain rounded-lg" />
+              <img src= {Products.image} alt={Products.title} className="w-full h-full object-contain rounded-lg" />
+       
             </div>
 
-            <h3 className="font-medium text-gray-800">{Products.title}</h3>
-             <p className="text-sm text-gray-600">{Products.category}</p> 
-            <div className="mt-2 flex items-center justify-between">
-           <span className="font-bold text-gray-800">${Products.price}</span>
+      <h3 className="font-medium text-gray-800">{Products.title.length>20?`${Products.title.slice(0,20)}...`: Products.title}</h3>
+        <p className="text-sm text-gray-600">{Products.category}</p> 
+      <div className="mt-2 flex items-center justify-between">
+         <span className="font-bold text-gray-800">${Products.price}</span>
               <span className="text-green-700 text-sm">40% off</span>
   
               <button className="px-4 py-2 text-sm font-medium text-white bg-orange-600 rounded-lg">Add to Cart</button>
