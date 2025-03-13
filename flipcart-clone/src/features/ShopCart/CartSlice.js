fc0326cb19bd38c3b643873ddb60f1448eb5ca15
kathirviswa <kathirviswa57@gmail.com>
2025-03-13 16:43:45 +0530

    g

---
+++ flipcart-clone/src/features/ShopCart/CartSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  products: [],
+  cart: [],
+}
+export const createSlice = createSlice({
+    name: 'products',
+    initialState,
+  reducers: {
+   Produ
+  },
+})
